90837ec8030b254fe315c95b8aa508d967b2b09a
minhle3107 <lexuanminh3107@gmail.com>
2024-11-20 09:11:35 +0700

    like

--- src/main/java/com/global/project/services/impl/LikeService.java
+++ src/main/java/com/global/project/services/impl/LikeService.java
@@ -9,21 +9,16 @@ import com.global.project.modal.LikeRequest;
 import com.global.project.repository.ILikeRepository;
 import com.global.project.services.ILikeService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import static com.global.project.configuration.jwtConfig.JwtAuthenticationFilter.getJwtFromRequest;
 
 @Service
 public class LikeService implements ILikeService {
 
-    @Value("${jwt.SECRET_ACCESS_TOKEN_KEY}")
-    private String JWT_SECRET_ACCESS_TOKEN;
-
     private final ILikeRepository likeRepository;
 
     private final JwtProvider jwtProvider;
+
 
     public LikeService(ILikeRepository likeRepository, JwtProvider jwtProvider) {
         this.likeRepository = likeRepository;
@@ -32,13 +27,12 @@ public class LikeService implements ILikeService {
 
     @Override
     public ResponseEntity<ApiResponse<LikeResponse>> likeOrUnlike(LikeRequest likeRequest, HttpServletRequest request) {
-        String jwtToken = getJwtFromRequest(request);
-
-        String username = jwtProvider.getKeyByValueFromJWT(JWT_SECRET_ACCESS_TOKEN, "username", jwtToken, String.class);
+        String username = jwtProvider.getUsernameContext();
 
         if (username == null) {
             throw new AppException(ErrorCode.ACCOUNT_NOT_FOUND);
         }
+
 
         return null;
 
